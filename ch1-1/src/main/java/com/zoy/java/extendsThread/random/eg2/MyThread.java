bf45ac2b8e9f55f017a0fe1cd4ed26893ac73f94
zoy2control <paulino_zoy_it@163.com>
2019-03-19 20:10:21 +0800

    thread ransdom 线程随机性 v0.1

--- ch1-1/src/main/java/com/zoy/java/extendsThread/random/eg2/MyThread.java
+++ ch1-1/src/main/java/com/zoy/java/extendsThread/random/eg2/MyThread.java
@@ -9,7 +9,7 @@ public class MyThread extends Thread{
         try {
             for(int i = 0; i < 10; i++) {
                 int time = (int) Math.random() * 1000;
-                Thread.sleep(time);// ·线程休眠，抛异常
+                Thread.sleep(time);// ·线程休眠，抛异常。通常用 休眠来阻止当前线程快速运行完所有代码，休眠从而使 CPU去执行 其他线程。模拟线程切换
                 System.out.println("run = " + Thread.currentThread().getName());
             }
         } catch (InterruptedException e) {
